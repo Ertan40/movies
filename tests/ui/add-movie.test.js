886e81446c4a1de5a3960b81e37316c9fb937e81
Ertan Donmez <ertandonmez1@gmail.com>
2024-04-13 21:07:50 +0300

    add .gitignore and update UI test files

--- tests/ui/add-movie.test.js
+++ tests/ui/add-movie.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check add movie page', async ({ page }) => {
-    await page.goto('https://localhost:8080');
+    await page.goto('http://localhost:8080/add-movie');
     const form = await page.$('form');
     expect(form).toBeTruthy();
   });
